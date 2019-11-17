9a8e8a158a30f7f49ed3f78832b42506b1355831
Михаил Грибанов <muchroom4427@yandex.ru>
2019-11-17 23:19:16 +0300

    Style fix

--- modules/task_2/gribanov_m_image_smoothing/image_smoothing.h
+++ modules/task_2/gribanov_m_image_smoothing/image_smoothing.h
@@ -1,13 +1,12 @@
 // Copyright 2019 Gribanov Mihail
 
-#ifndef MODULES_TASK_1_GRIBANOV_M_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
-#define MODULES_TASK_1_GRIBANOV_M_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
+#ifndef MODULES_TASK_2_GRIBANOV_M_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
+#define MODULES_TASK_2_GRIBANOV_M_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
 
 #include <vector>
-#include <windows.h>
 
 std::vector<int> getImg(int sizeX, int sizeY);
 std::vector<int> smoothSeq(std::vector<int> img, int sizeX, int sizeY);
 std::vector<int> smoothPar(std::vector<int> img, int sizeX, int sizeY);
 
-#endif  // MODULES_TASK_1_GRIBANOV_M_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
+#endif  // MODULES_TASK_2_GRIBANOV_M_IMAGE_SMOOTHING_IMAGE_SMOOTHING_H_
